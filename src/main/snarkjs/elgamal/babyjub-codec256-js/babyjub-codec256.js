780aaa21509aaae9ed68f1c26131140b40788e9d
dezhic <chen.dz@live.com>
2023-03-11 02:34:52 +0800

    add babyjub-codec [WIP]
    en/decode arbitrary 256-bit bigint to babyjub point + xor

---
+++ src/main/snarkjs/elgamal/babyjub-codec256-js/babyjub-codec256.js
@@ -0,0 +1,31 @@
+const crypto = require("crypto");
+const iden3crypto = require("@iden3/js-crypto");
+
+
+/**
+ * 
+ * @param {bigint} message an arbitrary 256-bit bigint
+ * @returns {point: [bigint, bigint], xmXor: bigint} point: the encoded babyjub point, xmXor: the xor of the message bytes and the x-coordinate of the point
+ */
+function encode(message) {
+    const point = randomPoint();
+    const xmXor = point[0] ^ message;
+    return { point: point, xmXor: xmXor };
+}
+
+/**
+ * 
+ * @param {[bigint, bigint]} point 
+ * @param {bigint} xmXor 
+ * @returns {bigint} the decoded message
+ */
+function decode(point, xmXor) {
+    
+    return new Uint8Array(32);
+}
+
+module.exports.randomPoint = function randomPoint() {
+    const privBuf = crypto.randomBytes(32);
+    const priv = new iden3crypto.PrivateKey(privBuf);
+    return priv.public().p;
+}
