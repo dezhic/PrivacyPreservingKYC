148f0736f6aa02a9f073ab0ec1d96138268f9211
dezhic <chen.dz@live.com>
2023-03-18 02:38:13 +0800

    update jsdoc of function sign

--- src/main/snarkjs/zkkyc/services/sign.js
+++ src/main/snarkjs/zkkyc/services/sign.js
@@ -4,7 +4,7 @@ const iden3crypto = require("@iden3/js-crypto");
  * 
  * @param {Uint8Array} privKey 
  * @param {bigint} msgHash 
- * @returns { R8: [bigint, bigint], S: bigint }
+ * @returns {{ R8: [bigint, bigint], S: bigint }}
  */
 function sign(privKey, msgHash) {
     return iden3crypto.eddsa.signPoseidon(privKey, msgHash);
