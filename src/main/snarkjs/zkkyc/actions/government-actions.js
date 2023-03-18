d27d4e1e7613ae91bc92f610934e4d9db02416fd
dezhic <chen.dz@live.com>
2023-03-18 16:05:11 +0800

    update js actions

--- src/main/snarkjs/zkkyc/actions/government-actions.js
+++ src/main/snarkjs/zkkyc/actions/government-actions.js
@@ -1,3 +1,22 @@
 module.exports = {
 
+    /**
+     * Generate babyjub public key and private key given a 32-byte hex string, or generate a new random key pair
+     * 
+     * @param {string | undefined} privKey hex string of a 32-byte buffer for generating a babyjub key pair, or undefined to generate a new random key
+     * @returns 
+     */
+    generateKeyPair: (privKey) => {
+        const keys = keygen.babyJubKeyGen(privKey);
+        return {
+            priv: Buffer.from(keys.priv.sk).toString('hex'),
+            pub: keys.pub.p.map(uint256ToHex),
+            privScalar: uint256ToHex(keys.privScalar),
+        }
+    },
+
+    decryptToken: (token, privKey) => {
+
+    }
+
 }
--- src/main/snarkjs/zkkyc/actions/verifier-actions.js
+++ src/main/snarkjs/zkkyc/actions/verifier-actions.js
@@ -1,12 +1,3 @@
 const keygen = require('../services/keygen');
 module.exports = {
-    /**
-     * Generate babyjub public key and private key
-     * @param {string} hex 32-byte hex string
-     * @returns {object}
-     */
-    generateKeyPair: (hex) => {
-        
-        return keygen.generateKeyPair(hex);
-    }
 }
