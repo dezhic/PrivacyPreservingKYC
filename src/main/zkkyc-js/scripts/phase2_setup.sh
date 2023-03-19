89bf5ed7ae85415264f8e4ee971c508ca8fb09e8
dezhic <chen.dz@live.com>
2023-03-20 03:39:37 +0800

    phase2 script: verbose contribute

--- src/main/zkkyc-js/scripts/phase2_setup.sh
+++ src/main/zkkyc-js/scripts/phase2_setup.sh
@@ -19,7 +19,7 @@ echo "Verifed powersOfTau28_hez_final_17.ptau checksum."
 echo "Phase 2 setup..."
 snarkjs groth16 setup generate_zkkyc_token.r1cs powersOfTau28_hez_final_17.ptau generate_zkkyc_token_0000.zkey -v
 echo "Phase 2 contribute (hardcoded enctropy for demo)..."
-snarkjs zkey contribute generate_zkkyc_token_0000.zkey generate_zkkyc_token.zkey --name="Hardcoded Contributor" --entropy="hardcoded_asldfkjalskdjflkwegfovnwen"
+snarkjs zkey contribute generate_zkkyc_token_0000.zkey generate_zkkyc_token.zkey --name="Hardcoded Contributor" --entropy="hardcoded_asldfkjalskdjflkwegfovnwen" -v
 echo "Exporting verification key..."
 snarkjs zkey export verificationkey generate_zkkyc_token.zkey verification_key.json
 popd
